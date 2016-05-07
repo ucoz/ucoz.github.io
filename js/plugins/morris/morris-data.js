93900ba081802bb576deff3617fe80a19a0d4898
ucoz <faqzero@gmail.com>
2016-05-07 21:43:51 +0300

    Update morris-data.js

--- js/plugins/morris/morris-data.js
+++ js/plugins/morris/morris-data.js
@@ -3,52 +3,52 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2016 Январь',
+            period: '1',
             iphone: 2666,
             ipad: null,
             itouch: 2647
         }, {
-            period: '2016 Февраль',
+            period: '2',
             iphone: 2778,
             ipad: 2294,
             itouch: 2441
         }, {
-            period: '2016 Март',
+            period: '3',
             iphone: 4912,
             ipad: 1969,
             itouch: 2501
         }, {
-            period: '2016 Апрель',
+            period: '4',
             iphone: 3767,
             ipad: 3597,
             itouch: 5689
         }, {
-            period: '2016 Май',
+            period: '5',
             iphone: 6810,
             ipad: 1914,
             itouch: 2293
         }, {
-            period: '2016 Июнь',
+            period: '6',
             iphone: 5670,
             ipad: 4293,
             itouch: 1881
         }, {
-            period: '2016 Июль',
+            period: '7',
             iphone: 4820,
             ipad: 3795,
             itouch: 1588
         }, {
-            period: '2016 Август',
+            period: '8',
             iphone: 15073,
             ipad: 5967,
             itouch: 5175
         }, {
-            period: '2016 Сентябрь',
+            period: '9',
             iphone: 10687,
             ipad: 4460,
             itouch: 2028
         }, {
-            period: '2016 Октябрь',
+            period: '10',
             iphone: 8432,
             ipad: 5713,
             itouch: 1791
